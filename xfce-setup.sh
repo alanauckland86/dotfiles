3d8f66a95256f5a5dbcce15ef7ee291ad3d75afe
Alan <alan.auckland86@gmail.com>
2021-03-28 10:43:55 +0100

    copy fonts

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env sh
 
+#VARIABLES 
+
+FONT_PATH="$HOME/.fonts"
+
+
+#END VARIABLES
+
+
+#START FUNCTIONS
+
+
+
+
+
+# START SCRIPT EXECUTION 
+if [ ! -d $FONT_PATH ] 
+  then 
+    echo "$FONT_PATH does not exist. Creating directory"
+    mkdir $FONT_PATH
+    echo "font directory created"
+    echo "Copying fonts"
+    cp $HOME/dotfiles/fonts/* $FONT_PATH
+  else 
+    echo "Font directory already exisits"
+    echo "Copying fonts"
+    cp $HOME/dotfiles/fonts/* $FONT_PATH
+fi
 
 # Set xfce wallpaper
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorrdp0/workspace0/last-image -s $HOME/dotfiles/images/wallpapers/anime-skies.jpg
+#xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorrdp0/workspace0/last-image -s $HOME/dotfiles/images/wallpapers/anime-skies.jpg
