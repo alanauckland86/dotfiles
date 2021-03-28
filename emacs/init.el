2352654faf53a6c142e703ce3335a0ef44060ab3
Alan <alan.auckland86@gmail.com>
2021-03-28 14:17:39 +0100

    install use-package if not available

--- emacs/init.el
+++ emacs/init.el
@@ -36,7 +36,24 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+
+;; install use-package if not installed on system
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
 ;; END PACKAGE MANAGMENT
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
