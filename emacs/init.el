339355062359403f37ee0f6287789e0f907944f4
Alan Auckland <alan.auckland@os.uk>
2021-09-27 17:38:36 +0100

    undo straight.el

--- emacs/init.el
+++ emacs/init.el
@@ -29,24 +29,6 @@
 ;; END EMACS USER INTERFACE
  
 
-;; START PACKAGE MANAGMENT WITH STRAIGHT.EL
-
-
-;; Bootstrap code
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(straight-use-package 'command-log-mode)
 
 ;; Mac OS
 
@@ -58,164 +40,144 @@
 ;; Make sure on MacOS to run 'brew install markdown' to enable markdown preview
 
 
-(straight-use-package 'markdown-mode)
-(straight-use-package 'treemacs)
-(straight-use-package 'magit)
-
-;; Set up spelling
-
-;; Set emacs to use installed markdown package from brew
-(custom-set-variables
-  '(markdown-command "/usr/local/bin/markdown"))
-
-(setq-default ispell-program-name "C:/bin/Aspell-en-0.50-2-3.exe")
-(setq text-mode-hook '(lamda() (flyspell-mode t) ))
-
-(straight-use-package 'treemacs)
-
-
-;; Spell checking
-;; Mac OS brew install hunspell
-
-(when (executable-find "hunspell")
-  (setq-default ispell-program-name "hunspell")
-  (setq ispell-really-hunspell t))
-
-
-;; require 'package) ;; Ensure we have the package "package" loaded to setup and manage packages in Emacs
+(require 'package) ;; Ensure we have the package "package" loaded to setup and manage packages in Emacs
 
 
 ;; Set up sources to get packages from. "package-archives" is a variable that holds the package source locations
 ;; which is why we are setting this.
 
-;;(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-;;                         ("org" . "https://orgmode.org/elpa/")
-;;                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;;(add-to-list 'package-archives
-;;             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 
 ;; prepare package system to be used
-;;(package-initialize)
+(package-initialize)
 
 ;;  check package-archive-contents is present
 ;; on first load look to see if there is an archive. If there is then get it
-;;(unless package-archive-contents
-;;  (package-refresh-contents))
+(unless package-archive-contents
+  (package-refresh-contents))
 
 
 ;; install use-package if not installed on system
-;;(when (not (package-installed-p 'use-package))
-;;  (package-refresh-contents)
-;;  (package-install 'use-package))
-;;(require 'use-package)
-;;(setq use-package-always-ensure t)
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 
 ;; Show commands entered in new window
 ;; ctl+x ctl+e to evaluate new package
 ;; meta+x comman-log-mode to run
 ;; Open command log =  M-x clm/open-command-log-buffer
-;;(use-package command-log-mode)
+(use-package command-log-mode)
 
 
 
-;;(use-package markdown-mode
-;;  :ensure t
-;; :commands (markdown-mode gfm-mode)
-;;  :mode (("README\\.md\\'" . gfm-mode)
-;;         ("\\.md\\'" . markdown-mode)
-;;         ("\\.markdown\\'" . markdown-mode))
-;;  :init (setq markdown-command "/usr/local/bin/pandoc"))
+(use-package markdown-mode
+  :ensure t
+ :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "/usr/local/bin/pandoc"))
 
 
-;;(use-package treemacs
-;;  :ensure t
- ;; :defer t
-;;  :init
-;;  (with-eval-after-load 'winum
- ;;   (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-;;  :config
-;;  (progn
-;;    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-;;          treemacs-deferred-git-apply-delay      0.5
-;;          treemacs-directory-name-transformer    #'identity
-;;          treemacs-display-in-side-window        t
-;;          treemacs-eldoc-display                 t
-;;          treemacs-file-event-delay              5000
-;;          treemacs-file-extension-regex          treemacs-last-period-regex-value
-;;          treemacs-file-follow-delay             0.2
-;;          treemacs-file-name-transformer         #'identity
-;;          treemacs-follow-after-init             t
-;;          treemacs-expand-after-init             t
-;;          treemacs-git-command-pipe              ""
-;;          treemacs-goto-tag-strategy             'refetch-index
-;;          treemacs-indentation                   2
-;;          treemacs-indentation-string            " "
-;;          treemacs-is-never-other-window         nil
-;;          treemacs-max-git-entries               5000
-;;          treemacs-missing-project-action        'ask
-;;          treemacs-move-forward-on-expand        nil
-;;          treemacs-no-png-images                 nil
-;;          treemacs-no-delete-other-windows       t
-;;          treemacs-project-follow-cleanup        nil
-;;          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-;;          treemacs-position                      'left
-;;          treemacs-read-string-input             'from-child-frame
-;;          treemacs-recenter-distance             0.1
-;;          treemacs-recenter-after-file-follow    nil
-;;          treemacs-recenter-after-tag-follow     nil
-;;         treemacs-recenter-after-project-jump   'always
-;;          treemacs-recenter-after-project-expand 'on-distance
-;;          treemacs-litter-directories            '("/node_modules" "/.venv" "/.cask")
-;;          treemacs-show-cursor                   nil
-;;          treemacs-show-hidden-files             t
-;;          treemacs-silent-filewatch              nil
-;;          treemacs-silent-refresh                nil
-;;          treemacs-sorting                       'alphabetic-asc
-;;          treemacs-space-between-root-nodes      t
-;;          treemacs-tag-follow-cleanup            t
-;;          treemacs-tag-follow-delay              1.5
-;;          treemacs-user-mode-line-format         nil
-;;          treemacs-user-header-line-format       nil
-;;          treemacs-width                         35
-;;          treemacs-workspace-switch-cleanup      nil)
-;;
-;;    ;; The default width and height of the icons is 22 pixels. If you are
-;;    ;; using a Hi-DPI display, uncomment this to double the icon size.
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-expand-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-read-string-input             'from-child-frame
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-litter-directories            '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35
+          treemacs-workspace-switch-cleanup      nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
     ;;(treemacs-resize-icons 44)
 
-;;    (treemacs-follow-mode t)
-;;    (treemacs-filewatch-mode t)
-;;    (treemacs-fringe-indicator-mode 'always)
-;;    (pcase (cons (not (null (executable-find "git")))
-;;                 (not (null treemacs-python-executable)))
-;;      (`(t . t)
-;;       (treemacs-git-mode 'deferred))
-;;      (`(t . _)
-;;       (treemacs-git-mode 'simple))))
-;;  :bind
-;;  (:map global-map
-;;        ("M-0"       . treemacs-select-window)
-;;        ("C-x t 1"   . treemacs-delete-other-windows)
-;;        ("C-x t t"   . treemacs)
-;;        ("C-x t B"   . treemacs-bookmark)
-;;        ("C-x t C-t" . treemacs-find-file)
-;;        ("C-x t M-t" . treemacs-find-tag)))
-;;
-;;
-;;(use-package treemacs-projectile
-;;  :after (treemacs projectile)
-;;  :ensure t)
-;;
-;;(use-package treemacs-icons-dired
-;;  :after (treemacs dired)
- ;; :ensure t
-;;  :config (treemacs-icons-dired-mode))
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
 
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after (treemacs dired)
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 
 
@@ -233,3 +195,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
